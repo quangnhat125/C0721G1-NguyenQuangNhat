e8ebd089da4334997102c9ecb24b97138c8e3810
quangnhatnguyen1405@gmail.com <quangnhatnguyen1405@gmail.com>
2021-07-20 00:05:25 +0700

    update

---
+++ Git Hub example/07_Bien_kieu_du_lieu_va_toan_tu/Ung_dung_may_tinh.js
@@ -0,0 +1,24 @@
+function do_something() {
+  let x = document.getElementById("number1").value;
+  let y = document.getElementById("number2").value;
+  let z = x + y;
+  document.getElementById("result").innerText = z;
+}
+function do_something1() {
+  let x = document.getElementById("number1").value;
+  let y = document.getElementById("number2").value;
+  let z = x - y;
+  document.getElementById("result").innerText = z;
+}
+function do_something2() {
+  let x = document.getElementById("number1").value;
+  let y = document.getElementById("number2").value;
+  let z = x * y;
+  document.getElementById("result").innerText = z;
+}
+function do_something3() {
+  let x = document.getElementById("number1").value;
+  let y = document.getElementById("number2").value;
+  let z = x / y;
+  document.getElementById("result").innerText = z;
+}
