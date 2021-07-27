685978457247ad5eefa95a738770cf54f39a4f9d
quangnhatnguyen1405@gmail.com <quangnhatnguyen1405@gmail.com>
2021-07-27 11:13:36 +0700

    update

--- Git Hub example/11_cau_truc_lap_2/hien_thi_cac_so_nguyen.js
+++ Git Hub example/11_cau_truc_lap_2/hien_thi_cac_so_nguyen.js
@@ -1,30 +1,32 @@
 let N = 2;
-    function kiem_tra_snt(n) {
-        var ketqua = true;
-        if (n < N) {
-            ketqua = false;
-        } else if (n == N) {
-            ketqua = true;
-        } else if (n % N == 0) {
-            ketqua = false;
-        } 
-            else {
-            for (var i = 3; i <= Math.sqrt(n); i += 2) {
-                if (n %  i == 0) {
-                    ketqua = false;
-                    break;
-                }
-            }
-        }
-        return ketqua;
-    }  
-    function click() {
-        let num = parseInt(document.getElementById("number")).value;
-        let count = 0;
-            for (count = 0, count < num; count++ ) {
-                if (kiem_tra_snt(n)){
-                    count += i + "</br>";
-                }
-            }
-            document.getElementById("result").innerHTML= count;
+function kiem_tra_snt(n) {
+  var ketqua = true;
+  if (n < 2) {
+    ketqua = false;
+  } else if (n == 2) {
+    ketqua = true;
+  } else if (n % 2 == 0) {
+    ketqua = false;
+  } else {
+    for (var i = 2; i <= Math.sqrt(n); i += 1) {
+      if (n % i == 0) {
+        ketqua = false;
+        break;
+      }
     }
+  }
+  return ketqua;
+}
+function print() {
+  let n = 2;
+  let num = parseInt(document.getElementById("number").value);
+  let count = 0;
+  while (count < num) {
+    if (kiem_tra_snt(n)) {
+      count += 1;
+      console.log(n);
+    }
+    n++;
+  }
+  document.getElementById("result").innerHTML = count;
+}
