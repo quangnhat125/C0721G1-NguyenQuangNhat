a7d2499a157f5eee5792e872d23a95a091a8340d
quangnhatnguyen1405@gmail.com <quangnhatnguyen1405@gmail.com>
2021-08-17 19:57:31 +0700

    update

--- module2/src/_02_Loop_In_Java/Bai_Tap/BaiTap3.java
+++ module2/src/_02_Loop_In_Java/Bai_Tap/BaiTap3.java
@@ -2,16 +2,21 @@ package _02_Loop_In_Java.Bai_Tap;
 
 public class BaiTap3 {
     public static void main(String[] args) {
-        for (int i = 2; i < 100; i++) {
-            int j;
-            for (j = 2; j < i; j++) {
-                if (i % j == 0) {
+        int N = 2;
+        while (N < 100) {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(N)) {
+                if (N % i == 0) {
+                    check = false;
                     break;
                 }
+                i++;
             }
-            if (i == j) {
-                System.out.print(j + " ");
+            if (check) {
+                System.out.println(N);
+            }
+            N++;
             }
         }
-    }
 }
