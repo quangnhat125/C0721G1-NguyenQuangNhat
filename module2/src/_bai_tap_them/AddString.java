b47a665d3fcc85b4b93ec3eb7e2ef00aea242669
quangnhatnguyen1405@gmail.com <quangnhatnguyen1405@gmail.com>
2021-08-24 19:39:04 +0700

    update

--- module2/src/_bai_tap_them/AddString.java
+++ module2/src/_bai_tap_them/AddString.java
@@ -1,33 +1,33 @@
-package _bai_tap_them;
-
-
-import java.util.Scanner;
-
-public class AddString {
-
-    public static void main(String[] args) {
-        String string;
-        int count;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap vao chuoi bat ki: ");
-        string = sc.nextLine();
-        find(string);
-
-    }
-
-    public static int find(String string) {
-        String[] array = new String[string.length()];
-        for (int i = 0; i < string.length() - 1; i++) {
-            for (int j = i + 1; j < string.length() - 1; j++) {
-                if (string.charAt(i) == string.charAt(j) && string.charAt(i) != ' ' && string.charAt(j) != ' ') {
-                  array[i] = String.valueOf(string.charAt(i));
-                  for(int k = i; k >= 0; k--) {
-                      array[i] = String.valueOf(string.charAt(i));
-                  }
-                }
-            }
-            System.out.println(array[i]);
-        }
-        return 1;
-    }
-}
+//package _bai_tap_them;
+//
+//
+//import java.util.Scanner;
+//
+//public class AddString {
+//
+//    public static void main(String[] args) {
+//        String string;
+//        int count;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhap vao chuoi bat ki: ");
+//        string = sc.nextLine();
+//        find(string);
+//
+//    }
+//
+//    public static int find(String string) {
+//        String[] array = new String[string.length()];
+//        for (int i = 0; i < string.length() - 1; i++) {
+//            for (int j = i + 1; j < string.length() - 1; j++) {
+//                if (string.charAt(i) == string.charAt(j) && string.charAt(i) != ' ' && string.charAt(j) != ' ') {
+//                  array[i] = String.valueOf(string.charAt(i));
+//                  for(int k = i; k >= 0; k--) {
+//                      array[i] = String.valueOf(string.charAt(i));
+//                  }
+//                }
+//            }
+//            System.out.println(array[i]);
+//        }
+//        return 1;
+//    }
+//}
